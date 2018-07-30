05ca7444696f68f2d20367092478276b0f4c3c31
Konstantin Greger <kogreger@gmail.com>
2018-07-30 10:23:55 +0200

    direct bash script to actual R script

--- extract-msd.sh
+++ extract-msd.sh
@@ -4,5 +4,6 @@ while true
 do
 	[[ `ls ~/Desktop/MSD/ | wc -l | wc -l` -gt 0 ]]
 	echo $COUNTER
+	/Library/Frameworks/R.framework/Resources/bin/Rscript "/Users/konstantingreger/Documents/GitHub/scrape-million-song-dataset/scrape-million-song-dataset.R"
 	let COUNTER=COUNTER+10000
 done
